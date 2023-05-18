11265c9fb923a5792506147ac497c3cb5c2ed512
PedroRcha <pedro.leonardo.pereira.rocha@gmail.com>
2023-05-18 11:03:55 -0300

    Como criar classe

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Aula introdutória de Orientação à Objetos (OO
+        // Aula introdutória de Orientação à Objetos (OO)
+        // Declaração de objeto
+        Pessoa adao;
+        // Instanciação do objeto
+        adao = new Pessoa();
+
+        // Ao mesmo tempo
+        Pessoa qualquer = new Pessoa();
+
+        //Definir formato do objeto
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "Lee";
+        qualquer.falar();
+        System.out.println(qualquer.falar("oi"));
+
 
 
     }
---
+++ src/Pessoa.java
@@ -0,0 +1,16 @@
+public class Pessoa {
+    // Atributos da classe
+    public String nome;         // objeto do tipo string
+    public String sobrenome;
+    // Métodos da classe
+    public void falar(){        // void == sem retorno
+        System.out.println("falei");
+    }
+    public String falar(String texto){
+        return texto;
+    }
+
+
+
+
+}
