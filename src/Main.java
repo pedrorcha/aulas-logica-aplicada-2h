b037e39e05e229ae7b6bb509c330696b58015f1e
PedroRcha <pedro.leonardo.pereira.rocha@gmail.com>
2023-05-18 09:33:15 -0300

    Aula introdutória de Orientação à Objetos (OO)

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        int contador = 0;
+     // Aula introdutória de Orientação à Objetos (OO)
 
-        // laço while
-        while(contador < 10){
-            System.out.println("Contador = " + contador);
-            // Incremento
-            contador++; // OU contador = contador + 1
-        }
-
-        // Laço for
-        for(int x = 0; x < 10; x++){
-            System.out.println("Contador = " + x);
-        }
-
-        // Laço do while
-        int cont = 1;
-        do{
-            // processo
-            System.out.println(cont);
-            // incremento
-            cont++;
-
-        }while(cont <= 10);
 
     }
 }
